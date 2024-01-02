3702854a04c72c6b95c1f5c811cd09bbbc50957c
chfhr22 <144635651+chfhr22@users.noreply.github.com>
2024-01-03 06:05:56 +0900

    0103

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDse6pxpD1OMgEIFyaqpNe0d24gTueWI3o",
+  authDomain: "gr22n-portfolio.firebaseapp.com",
+  projectId: "gr22n-portfolio",
+  storageBucket: "gr22n-portfolio.appspot.com",
+  messagingSenderId: "52973717523",
+  appId: "1:52973717523:web:a8331a43ba06d9a0eed7e6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
